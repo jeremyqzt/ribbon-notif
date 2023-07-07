b4123812eedcebe671aaf9004327b1decd3c0278
Jeremy Qian <jq357@hotmail.com>
2023-07-06 20:54:30 -0400

    init app

--- src/main/java/com/ribbon/notif/NotifApplication.java
+++ src/main/java/com/ribbon/notif/NotifApplication.java
@@ -2,6 +2,11 @@ package com.ribbon.notif;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import graphql.Scalars;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLSchema;
 
 @SpringBootApplication
 public class NotifApplication {
---
+++ src/main/java/com/ribbon/notif/datafetchers/Show.java
@@ -0,0 +1,19 @@
+package com.ribbon.notif.datafetchers;
+
+public class Show {
+    private final String title;
+    private final Integer releaseYear;
+
+    public Show(String title, Integer releaseYear) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+}
