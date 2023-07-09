3e18ab5fe11f852ac06a483644838e3cde5e7b6c
Jeremy Qian <jq357@hotmail.com>
2023-07-09 14:02:49 -0400

    make app run again

--- src/main/java/com/ribbon/notif/services/NotificationService.java
+++ src/main/java/com/ribbon/notif/services/NotificationService.java
@@ -1,10 +1,20 @@
 package com.ribbon.notif.services;
 
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class NotificationService {
     public void sendNotif() {
+        WebClient webClient = WebClient.builder()
+            .baseUrl("http://localhost:3000")
+            .defaultCookie("cookie-name", "cookie-value")
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
+
         return;
     }
 }
