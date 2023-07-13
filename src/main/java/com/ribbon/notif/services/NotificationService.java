f49131be77416073a62d0146f7723038a880c6ad
Jeremy Qian <jq357@hotmail.com>
2023-07-13 14:22:10 -0400

    initial client firing

--- src/main/java/com/ribbon/notif/services/NotificationService.java
+++ src/main/java/com/ribbon/notif/services/NotificationService.java
@@ -1,6 +1,5 @@
 package com.ribbon.notif.services;
 
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -9,25 +8,38 @@ import org.springframework.web.reactive.function.client.WebClient;
 import com.ribbon.notif.config.Environ;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class NotificationService {
     private final Environ environment;
-    
+
     public void sendNotif() {
 
         String apiKey = environment.getApiKey();
-        System.out.println(apiKey);
-        System.out.println(apiKey);
-        System.out.println(apiKey);
+    
 
         WebClient webClient = WebClient.builder()
-            .baseUrl("http://localhost:3000")
-            .defaultCookie("cookie-name", "cookie-value")
-            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .build();
+                .baseUrl("https://api.elasticemail.com")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
 
+        Mono<String> response = webClient
+                .get()
+                .uri(builder -> {
+                    return builder.path("/v2/email/send")
+                            .queryParam("apikey", apiKey)
+                            .queryParam("to", "jq357@hotmail.com")
+                            .queryParam("template", "recovery")
+                            .queryParam("from", "no-reply@ribbonreceipts.com")
+                            .build();
+                }).retrieve()
+                .bodyToMono(String.class);
+
+        log.debug("email sent: {}", response.block());
         return;
     }
 }
