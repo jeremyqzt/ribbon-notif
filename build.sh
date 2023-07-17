c203fb987c0ad5f4624bcf624f95e9e59d60082e
Jeremy Qian <jq357@hotmail.com>
2023-07-16 20:00:32 -0400

    nicer build

--- build.sh
+++ build.sh
@@ -1,4 +1,22 @@
 
+#!/bin/sh
+
 mvn clean install -DskipTests
 
-docker build -f Docker/Dockerfile --build-arg JAR_FILE=target/*.jar -t jeremyqzt/ribbon-notif .
+VERSION=$(git log -1 --pretty=%h)
+
+#docker build -f Docker/Dockerfile --build-arg JAR_FILE=target/*.jar -t jeremyqzt/ribbonnotif .
+
+
+if [ -z "$1" ]
+  then
+    echo "Native build\n"
+    docker build -f Docker/Dockerfile --build-arg JAR_FILE=target/*.jar -t jeremyqzt/ribbonnotif .
+    exit 0
+fi
+
+echo "Buildx build\n"
+eval "docker buildx build --push --platform=linux/arm64,linux/amd64 -f Docker/Dockerfile -t jeremyqzt/ribbonnotif:$VERSION ."
+
+
+IMAGE="jeremyqzt/ribbonnotif:$VERSION" envsubst < k8s/notif.yaml | kubectl apply -f -
